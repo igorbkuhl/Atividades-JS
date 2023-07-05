d9ca5188b3522166815115245110b01c6eb54062
igorbkuhl <igorb.kuhl@gmail.com>
2023-07-05 07:20:57 -0300

    Pequeno ajuste no visual da página.

--- Exercícios JS/10 - keydown/script.js
+++ Exercícios JS/10 - keydown/script.js
@@ -8,8 +8,8 @@ function iniciar() {
 var gameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 500;
+        this.canvas.height = 350;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(mover, 20);
