f8065c515f30aedeabca08aef8e8006bff9f7452
Igor Borges <igorb.kuhl@gmail.com>
2023-08-31 13:31:00 -0300

    Script aprimorado.

--- Exercícios JS/2 - Contagem de votos/script.js
+++ Exercícios JS/2 - Contagem de votos/script.js
@@ -1,23 +1,24 @@
-let a = 800;
-let b = 300;
-let c = 700;
-let d = 500;
-let e = 400;
-let x = 60;
-let y = 40;
-const total = a + b + c + d + e + x + y;
+let eleitos = [
+    regisDaCosta = 800,
+    pauloMachado = 300,
+    mariaPinho = 700,
+    angelaMartins = 500,
+    mateusSilverio = 400,
+    votosBrancos = 60,
+    votosNulos = 40
+]
 
+const total = eleitos[0] + eleitos[1] + eleitos[2] + eleitos[3] + eleitos[4] + eleitos[5] + eleitos[6];
 
 function calcularPorcentagem(x, y) {
     return ((x / y) * 100).toFixed(1);
 }
 
+document.getElementById("regisDaCosta").innerHTML = (calcularPorcentagem(eleitos[0], total) + "%");
+document.getElementById("pauloMachado").innerHTML = (calcularPorcentagem(eleitos[1], total) + "%");
+document.getElementById("mariaPinho").innerHTML = (calcularPorcentagem(eleitos[2], total) + "%");
+document.getElementById("angelaMartins").innerHTML = (calcularPorcentagem(eleitos[3], total) + "%");
+document.getElementById("mateusSilvério").innerHTML = (calcularPorcentagem(eleitos[4], total) + "%");
 
-document.getElementById("regisDaCosta").innerHTML = (calcularPorcentagem(a, total) + "%");
-document.getElementById("pauloMachado").innerHTML = (calcularPorcentagem(b, total) + "%");
-document.getElementById("mariaPinho").innerHTML = (calcularPorcentagem(c, total) + "%");
-document.getElementById("angelaMartins").innerHTML = (calcularPorcentagem(d, total) + "%");
-document.getElementById("mateusSilvério").innerHTML = (calcularPorcentagem(e, total) + "%");
-
-document.getElementById("votosBrancos").innerHTML = (calcularPorcentagem(x, total) + "%");
-document.getElementById("votosNulos").innerHTML = (calcularPorcentagem(y, total) + "%");
+document.getElementById("votosBrancos").innerHTML = (calcularPorcentagem(eleitos[5], total) + "%");
+document.getElementById("votosNulos").innerHTML = (calcularPorcentagem(eleitos[6], total) + "%");
