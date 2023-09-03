25849d8476deadac7839b5f5678eb325867200f6
Igor Borges <igorb.kuhl@gmail.com>
2023-09-03 20:14:10 -0300

    Verificação das operações aprimorada.

--- Exercícios JS/4 - Calculadora prompt/script.js
+++ Exercícios JS/4 - Calculadora prompt/script.js
@@ -1,26 +1,35 @@
-var firstValue, operation, secondValue, result;
-
+let firstValue, operation, secondValue, result;
 
 while(true) {
+    
+    // Informa o primeiro valor
+    firstValue = parseFloat(prompt("Insira o primeiro valor a ser calculado: "));
 
-    firstValue = parseFloat(prompt("Insira o primeiro valor a ser calculado:"));
-    operation = prompt("Insira a operação desejada:");
-    secondValue = parseFloat(prompt("Insira o segundo valor a ser calculado:"));
-    result = (firstValue + operation + secondValue) + " = " ;
+    // Informa a operação
+    operation = prompt("Insira a operação desejada: ");
 
-    if(operation == "+") {
-        result = Number(firstValue) + Number(secondValue);
-    } else if (operation == "-") {
-        result = Number(firstValue) - Number(secondValue);
-    } else if (operation == "*") {
-        result = Number(firstValue) * Number(secondValue);
-    } else if (operation == "/") {
-        result = Number(firstValue) / Number(secondValue);
+    // Informa o segundo valor
+    secondValue = parseFloat(prompt("Insira o segundo valor a ser calculado: "));
+
+    switch(operation) {
+        case '+':
+            result = Number(firstValue) + Number(secondValue);
+            break;
+        case '-':
+            result = Number(firstValue) - Number(secondValue);
+            break;
+        case '*':
+            result = Number(firstValue) * Number(secondValue);
+            break;
+        case '/':
+            result = Number(firstValue) / Number(secondValue);
+            break;
+        default:
+            alert('Insira uma operação com uma das opções: +, -, *, /');
+            break;
     }
 
-
-
-    window.alert("Resultado: " + result);
+    alert(`Resultado: ${result}`);
 
     if (!confirm("Deseja realizar mais um cálculo?")) {
         break;
