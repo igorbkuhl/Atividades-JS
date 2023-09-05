27789c2281a63c698f0234f27d2661ae46eb5b48
igorbkuhl <igorb.kuhl@gmail.com>
2023-09-05 13:28:19 -0300

    Pequenas mudanças na estrutura do script.

--- Exercícios JS/6 - Sorteio/script.js
+++ Exercícios JS/6 - Sorteio/script.js
@@ -1,16 +1,15 @@
 // Cria uma array e um contador
-var participants = [];
-var counter = 0;
+let participants = [];
+let counter = 0;
 
 // Declara a função do sorteio
-function raffle(winner) {
+const shuffle = (winner) => {
   // Inicia o loop de solicitações
   while(true) {
 
     // Cria uma array com o contador
     participants[counter] = {};
     participants[counter].name = prompt('Insira o nome do ' + (counter + 1) + 'º participante.');
-
     // Encerrando o loop de solicitações
     if(!confirm('Deseja adicionar mais um participante?')) {
       break;
@@ -21,14 +20,15 @@ function raffle(winner) {
 
   // Informa o vencedor do sorteio
   if (participants.length >= 2) {
-    var randomIndex = Math.floor(Math.random() * participants.length);
-    var sortedValue = participants[randomIndex].name;
+    let randomIndex = Math.floor(Math.random() * participants.length);
+    let sortedValue = participants[randomIndex].name;
 
-    console.log(`O vencedor do sorteio é ${sortedValue}!`);
-    winner = `O vencedor do sorteio é ${sortedValue}!`;
+    console.log(`O vencedor do sorteio é... ${sortedValue}!`);
+    winner = `O vencedor do sorteio é...<br/>${sortedValue}!`;
+    document.querySelector('p#winnerText').innerHTML = winner;
   } else {
     alert("Informe no mínimo dois participantes.");
   }
 }
 
-raffle();
+shuffle();
