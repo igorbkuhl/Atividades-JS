a6f4e7bff6fda1b71d5f501f144fd44abc0f39bb
igorbkuhl <igorb.kuhl@gmail.com>
2023-09-06 07:28:45 -0300

    Imported sidebar and added styles

--- atividades/src/App.vue
+++ atividades/src/App.vue
@@ -1,13 +1,47 @@
-<script setup lang="ts">
-
+<script lang="ts" setup>
+import Sidebar from './components/Sidebar.vue'
 </script>
 
 <template>
   <div class="app">
+    <Sidebar />
     <router-view />
   </div>
 </template>
 
 <style lang="sass">
+:root
+  --primary: #4ade80
+  --grey: #64748b
+  --dark: #1e293b
+  --dark-alt: #334155
+  --light: #f1f5f9
+  --background: #242424
+  --sidebar-width: 300px
 
+*
+  margin: 0
+  padding: 0
+  box-sizing: border-box
+  font-family: 'Fira sans', sans-serif
+
+body
+  background: var(--background)
+
+button
+  cursor: pointer
+  appearance: none
+  border: none
+  outline: none
+  background: none
+
+.app
+  display: flex
+
+  main
+    flex: 1 1 0
+    padding: 2rem
+
+    @media (max-width: 768px)
+      padding-left: 6rem
 </style>
