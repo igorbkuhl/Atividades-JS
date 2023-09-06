97bf00f53f7a75a2a254c547b09e553cbf3697d4
igorbkuhl <igorb.kuhl@gmail.com>
2023-09-06 07:28:06 -0300

    Temperature converter in process

---
+++ atividades/src/views/JavaScript/Atividade_JS_1.vue
@@ -0,0 +1,128 @@
+<script lang="ts">
+export default {
+    name: 'Conversor',
+    methods: {
+        conversor(source: string, valNum: number) {
+            let fahrenheit = (<HTMLInputElement>document.getElementById("fahrenheit")).value;
+            let celsius = (<HTMLInputElement>document.getElementById("celsius")).value;
+            let kelvin = (<HTMLInputElement>document.getElementById("kelvin")).value;
+
+            if (source == "fahrenheit") {
+                celsius = ((valNum - 32) / 1.8).toFixed(2);
+                kelvin = (((valNum - 32) /1.8) + 273.15).toFixed(2);
+            }
+            if (source == "celsius") {
+                fahrenheit = ((valNum * 1.8) + 32).toFixed(2);
+                kelvin = ((valNum) + 273.15).toFixed(2);
+            }
+            if (source == "kelvin") {
+                fahrenheit = (((valNum - 273.15) * 1.8) + 32).toFixed(2);
+                celsius = ((valNum) - 273.15).toFixed(2);
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div id="formArea">
+            <h1>Conversor de temperaturas</h1>
+            <form name="converter" action="#" method="get">
+                <div class="fahrenheit temp-area">
+                    <label for="fahrenheit">Fahrenheit</label>
+                    <input type="number" name="fahrenheit" id="fahrenheit" placeholder="Valor em Fahrenheit" onchange="conversor(this.id, this.value)"/> 
+                </div>
+                <br/>
+                <div class="celsius temp-area">
+                    <label for="celsius">Celsius</label>
+                    <input type="number" name="celsius" id="celsius" placeholder="Valor em Celsius" onchange="conversor(this.id, this.value)"/>
+                </div>
+                <br/>
+                <div class="kelvin temp-area">
+                    <label for="kelvin">Kelvin</label>
+                    <input type="number" name="kelvin" id="kelvin" placeholder="Valor em Kelvin" onchange="conversor(this.id, this.value)"/>
+                </div>
+            </form>
+        </div>
+        <div id="buttonArea">
+            <button id="botao" onclick="conversor(source, valNum)">Converter</button>
+        </div>
+</template>
+
+<style lang="sass" scoped>
+*
+    color: #f39ba3
+
+html
+    display: flex
+    flex-direction: column
+    flex-wrap: wrap
+
+body
+    background-color: #513340
+    margin-top: 20vh
+    margin-bottom: auto
+    margin-right: auto
+    margin-left: auto
+    padding: auto
+
+main
+    border-radius: 10px
+    display: flex
+    justify-content: center
+    flex-direction: column
+    background-color: #f5d4d6
+    padding: 8vh 0px
+
+h1
+    font-family: 'Courier New', Courier, monospace
+    font-size: 1.5em
+    font-weight: bold
+    text-align: center
+    text-transform: uppercase
+
+form
+    display: grid
+    justify-content: space-around
+    margin: 0
+    width: 600px
+
+label
+    font-family: 'Courier New', Courier, monospace
+    font-size: 0.9em
+    font-style: italic
+
+input
+    background-color: #946064
+    border: solid 1px
+    border-radius: 5px
+    padding: 5px 15px
+    text-align: center
+    width: 80%
+
+.temp-area
+    margin-top: 2vh
+
+#buttonArea
+    display: flex
+    justify-content: center
+    margin: auto
+
+#botao
+    background-color: #946064
+    border: 1px solid
+    border-radius: 4px
+    font-size: 18px
+    padding: 10px
+    cursor: pointer
+    display: inherit
+    justify-content: inherit
+    margin-top: 4vh
+
+::placeholder
+    color: #f39ba3
+
+::selection
+    background-color: #f39ba2a4
+    color: #f0839acb
+</style>
