7586042294a1520dbc924c7b6e77cb28a0a0565e
igorbkuhl <igorb.kuhl@gmail.com>
2023-09-09 06:33:46 -0300

    Added main view

---
+++ atividades/src/views/MainView.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Conversor from './JavaScript/Atividade_JS_1.vue'
+import Sidebar from '../components/Sidebar.vue'
+
+export default defineComponent({
+    name: 'MainView',
+    components: Conversor
+})
+</script>
+
+<template>
+    <Sidebar />
+    <Conversor />
+</template>
