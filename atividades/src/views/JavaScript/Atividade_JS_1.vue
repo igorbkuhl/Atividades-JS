4b801698b3c7748612235170b5a6083c3b7db7a4
igorbkuhl <igorb.kuhl@gmail.com>
2023-09-11 18:29:26 -0300

    Calculation working partially

--- atividades/src/views/JavaScript/Atividade_JS_1.vue
+++ atividades/src/views/JavaScript/Atividade_JS_1.vue
@@ -1,157 +1,163 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-    name: 'Conversor',
-    data() {
-        return {
-            source: '',
-            valNum: 0,
-            items: [
-                {id: 'fahrenheit', value: 0},
-                {id: 'celsius', value: 0},
-                {id: 'kelvin', value: 0}
-            ]
-        }
-    },
-    methods: {
-        conversor(source: string, valNum: number) {
-            let fahrenheit = (<HTMLInputElement>document.getElementById("fahrenheit")).value;
-            let celsius = (<HTMLInputElement>document.getElementById("celsius")).value;
-            let kelvin = (<HTMLInputElement>document.getElementById("kelvin")).value;
+import { defineComponent } from "vue"
 
-            if (source == "fahrenheit") {
-                celsius = ((valNum - 32) / 1.8).toFixed(2);
-                kelvin = (((valNum - 32) /1.8) + 273.15).toFixed(2);
-            }
-            if (source == "celsius") {
-                fahrenheit = ((valNum * 1.8) + 32).toFixed(2);
-                kelvin = ((valNum) + 273.15).toFixed(2);
-            }
-            if (source == "kelvin") {
-                fahrenheit = (((valNum - 273.15) * 1.8) + 32).toFixed(2);
-                celsius = ((valNum) - 273.15).toFixed(2);
-            }
-        }
+export default defineComponent ({
+  name: "Atividade JS 1",
+  data() {
+    return {
+			// source: String,
+			// valNum: Number,
+			tempF: 0,
+			tempC: 0,
+			tempK: 0,
     }
+		// type inputTypes = {
+		// 	id: string;
+		// 	value: number;
+		// }
+  },
+	methods: {
+		conversor(source: string, valNum: number) {
+			let fahrenheit = (document.querySelector("input#fahrenheit") as HTMLInputElement).value;
+			let celsius = (document.querySelector("input#celsius") as HTMLInputElement).value;
+			let kelvin = (document.querySelector("input#kelvin") as HTMLInputElement).value;
+			this.tempF = Number(fahrenheit);
+			this.tempC = Number(celsius);
+			this.tempK = Number(kelvin);
+
+			if (source == "fahrenheit") {
+				this.tempC = ((valNum - 32) / 1.8);
+				this.tempK = (((valNum - 32) / 1.8) + 273.15);
+			} 
+			
+			if (source == "celsius") {
+				this.tempF = ((valNum * 1.8) + 32);
+				this.tempK = ((valNum) + 273.15);
+			}
+
+			if (source = "kelvin") {
+				this.tempF = (((valNum - 273.15) * 1.8) + 32);
+				this.tempC = ((valNum) - 273.15);
+			}
+		}
+	}
 })
 </script>
 
 <template>
-    <main id="temp-converter">
-        <div id="formArea">
-            <h1>Conversor de temperaturas</h1>
-            <form action="#" method="get">
-                <div class="fahrenheit temp-area">
-                    <label for="fahrenheit">Fahrenheit</label>
-                    <input
-                    type="number"
-                    id="fahrenheit"
-                    placeholder="Valor em Fahrenheit"
-                    v-model="items[0].value"
-                    @change="conversor(items[0].id, items[0].value)"/> 
-                </div>
-                <br/>
-                <div class="celsius temp-area">
-                    <label for="celsius">Celsius</label>
-                    <input
-                    type="number"
-                    id="celsius"
-                    placeholder="Valor em Celsius"
-                    v-model="items[1].value"
-                    @change="conversor(items[1].id, items[1].value)"/>
-                </div>
-                <br/>
-                <div class="kelvin temp-area">
-                    <label for="kelvin">Kelvin</label>
-                    <input
-                    type="number"
-                    id="kelvin"
-                    placeholder="Valor em Kelvin"
-                    v-model="items[2].value"
-                    @change="conversor(items[2].id, items[2].value)"/>
-                </div>
-            </form>
-        </div>
-        <div id="buttonArea">
-            <button id="botao" @click="conversor(source, valNum)">Converter</button>
-        </div>
-    </main>
+  <main id="temp-converter">
+    <div id="formArea">
+      <h1>Conversor de temperaturas</h1>
+        <form action="#" method="get">
+          <div class="fahrenheit temp-area">
+            <label for="fahrenheit">Fahrenheit</label>
+            <input
+            type="number"
+            id="fahrenheit"
+            placeholder="Valor em Fahrenheit"
+            v-model="tempF"
+            @change="conversor(this.id, this.value)"/> 
+          </div>
+          <br/>
+          <div class="celsius temp-area">
+            <label for="celsius">Celsius</label>
+              <input
+              type="number"
+              id="celsius"
+              placeholder="Valor em Celsius"
+              v-model="tempC"
+              @change="conversor(this.id, this.value)"/>
+          </div>
+          <br/>
+          <div class="kelvin temp-area">
+              <label for="kelvin">Kelvin</label>
+              <input
+              type="number"
+              id="kelvin"
+              placeholder="Valor em Kelvin"
+              v-model="tempK"
+              @change="conversor(this.id, this.value)"/>
+          </div>
+        </form>
+    </div>
+    <div id="buttonArea">
+      <button id="botao" @click="conversor(source, valNum)">Converter</button>
+    </div>
+  </main>
 </template>
 
 <style lang="sass" scoped>
 *
-    color: #f39ba3
+  color: #f39ba3
 
 html
-    display: flex
-    flex-direction: column
-    flex-wrap: wrap
+  display: flex
+  flex-direction: column
+  flex-wrap: wrap
 
 body
-    background-color: #513340
-    margin-top: 20vh
-    margin-bottom: auto
-    margin-right: auto
-    margin-left: auto
-    padding: auto
+  margin-top: 20vh
+  margin-bottom: auto
+  margin-right: auto
+  margin-left: auto
+  padding: auto
 
 main
-    border-radius: 10px
-    display: flex
-    justify-content: center
-    flex-direction: column
-    background-color: #f5d4d6
-    padding: 8vh 0px
+  display: flex
+  justify-content: center
+  flex-direction: column
+  background-color: #513340
+  padding: 8vh 0px
 
 h1
-    font-family: 'Courier New', Courier, monospace
-    font-size: 1.5em
-    font-weight: bold
-    text-align: center
-    text-transform: uppercase
+  font-family: 'Courier New', Courier, monospace
+  font-size: 1.5em
+  font-weight: bold
+  text-align: center
+  text-transform: uppercase
 
 form
-    display: grid
-    justify-content: space-around
-    margin: 0
-    width: 600px
+  display: grid
+  justify-content: space-around
+  margin: 0
+  width: 600px
 
 label
-    font-family: 'Courier New', Courier, monospace
-    font-size: 0.9em
-    font-style: italic
+  font-family: 'Courier New', Courier, monospace
+  font-size: 0.9em
+  font-style: italic
 
 input
-    background-color: #946064
-    border: solid 1px
-    border-radius: 5px
-    padding: 5px 15px
-    text-align: center
-    width: 80%
+  background-color: #946064
+  border: solid 1px
+  border-radius: 5px
+  padding: 5px 15px
+  text-align: center
+  width: 80%
 
 .temp-area
-    margin-top: 2vh
+  margin-top: 2vh
 
 #buttonArea
-    display: flex
-    justify-content: center
-    margin: auto
+  display: flex
+  justify-content: center
+  margin: auto
 
 #botao
-    background-color: #946064
-    border: 1px solid
-    border-radius: 4px
-    font-size: 18px
-    padding: 10px
-    cursor: pointer
-    display: inherit
-    justify-content: inherit
-    margin-top: 4vh
+  background-color: #946064
+  border: 1px solid
+  border-radius: 4px
+  font-size: 18px
+  padding: 10px
+  cursor: pointer
+  display: inherit
+  justify-content: inherit
+  margin-top: 4vh
 
 ::placeholder
-    color: #f39ba3
+  color: #f39ba3
 
 ::selection
-    background-color: #f39ba2a4
-    color: #f0839acb
+  background-color: #f39ba2a4
+  color: #f0839acb
 </style>
