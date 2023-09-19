1af86c4db94252a4da9e34cacf38acb85f059968
igorbkuhl <igorb.kuhl@gmail.com>
2023-09-19 09:07:00 -0300

    Some styles moved to main CSS file.

--- src/views/JavaScript/Atividade_JS_1.vue
+++ src/views/JavaScript/Atividade_JS_1.vue
@@ -20,7 +20,7 @@ export default defineComponent ({
       
 			let fahrenheit = (document.querySelector("input#fahrenheit") as HTMLInputElement).value;
 			let celsius = (document.querySelector("input#celsius") as HTMLInputElement).value;
-			let kelvin = (document.querySelector("input#kelvin") as HTMLInputElement).value;
+			let kelvin = (<HTMLInputElement>document.querySelector("input#kelvin")).value;
 			this.tempF = parseFloat(fahrenheit);
 			this.tempC = parseFloat(celsius);
 			this.tempK = parseFloat(kelvin);
@@ -88,128 +88,80 @@ export default defineComponent ({
       this.srcT = this.srcK
     }
 	}
-  // fahrenheitInput(): void {
-  //   this.srcT = "fahrenheit"
-  // },
-  // cesiusInput(): void {
-  //   this.srcT = "celsius"
-  // },
-  // kelvinInput(): void {
-  //   this.srcT = "kelvin"
-  // }
 })
 </script>
 
 <template>
   <main id="temp-converter">
-    <div id="formArea">
+    <div class="main-area">
       <h1>Conversor de temperaturas</h1>
-        <form action="#" method="get">
-          <div class="fahrenheit temp-area">
-            <label for="fahrenheit">Fahrenheit</label>
-            <div class="temp-inputs">
-              <input
-                type="number"
-                id="fahrenheit"
-                placeholder="Valor em Fahrenheit"
-                v-model="tempF"
-                @change="conversor(srcF, tempF)"
-              />
-              <input type="button" value="+" id="increaseF" class="increase-button" @click="increaseFahrenheit()" />
-              <input type="button" value="-" id="decreaseF" class="decrease-button" @click="decreaseFahrenheit()" />
-            </div>
-          </div>
-          <div class="celsius temp-area">
-            <label for="celsius">Celsius</label>
-            <div class="temp-inputs">
-              <input
-                type="number"
-                id="celsius"
-                placeholder="Valor em Celsius"
-                v-model="tempC"
-                @change="conversor(srcC, tempC)"
-              />
-              <input type="button" value="+" id="increaseC" class="increase-button" @click="increaseCelsius()" />
-              <input type="button" value="-" id="decreaseC" class="decrease-button" @click="decreaseCelsius()" />
-            </div>
-          </div>
-          <div class="kelvin temp-area">
-              <label for="kelvin">Kelvin</label>
-              <div class="temp-inputs">
-                <input
-                  type="number"
-                  id="kelvin"
-                  placeholder="Valor em Kelvin"
-                  v-model="tempK"
-                  @change="conversor(srcK, tempK)"
-                />
-              <input type="button" value="+" id="increaseK" class="increase-button" @click="increaseKelvin()" />
-              <input type="button" value="-" id="decreaseK" class="decrease-button" @click="decreaseKelvin()" />
-            </div>
-          </div>
-        </form>
-    </div>
+      <div class="fahrenheit temp-area">
+        <label for="fahrenheit">Fahrenheit</label>
+        <div class="temp-inputs">
+          <input
+            type="number"
+            id="fahrenheit"
+            placeholder="Valor em Fahrenheit"
+            v-model="tempF"
+            @change="conversor(srcF, tempF)"
+          />
+          <input type="button" value="+" id="increaseF" class="increase-button" @click="increaseFahrenheit()" />
+          <input type="button" value="–" id="decreaseF" class="decrease-button" @click="decreaseFahrenheit()" />
+        </div>
+      </div>
+      <div class="celsius temp-area">
+        <label for="celsius">Celsius</label>
+        <div class="temp-inputs">
+          <input
+            type="number"
+            id="celsius"
+            placeholder="Valor em Celsius"
+            v-model="tempC"
+            @change="conversor(srcC, tempC)"
+          />
+          <input type="button" value="+" id="increaseC" class="increase-button" @click="increaseCelsius()" />
+          <input type="button" value="–" id="decreaseC" class="decrease-button" @click="decreaseCelsius()" />
+        </div>
+      </div>
+      <div class="kelvin temp-area">
+          <label for="kelvin">Kelvin</label>
+          <div class="temp-inputs">
+            <input
+              type="number"
+              id="kelvin"
+              placeholder="Valor em Kelvin"
+              v-model="tempK"
+              @change="conversor(srcK, tempK)"
+            />
+          <input type="button" value="+" id="increaseK" class="increase-button" @click="increaseKelvin()" />
+          <input type="button" value="–" id="decreaseK" class="decrease-button" @click="decreaseKelvin()" />
+        </div>
+      </div>
     <div id="buttonArea">
       <button id="botao" @click="conversor(srcT, tempT)">Converter</button>
+    </div>
     </div>
   </main>
 </template>
 
 <style lang="sass" scoped>
-*
-  color: #f39ba3
-
-body
-  margin: 20vh auto auto auto
-  padding: auto
-
-main
-  background-color: #513340
-  display: flex
-  justify-content: center
-  flex-direction: column
-  padding: 8vh 0px
-  height: 100vh
-
-h1
-  font-family: 'Courier New', Courier, monospace
-  font-size: 1.5em
-  font-weight: bold
-  text-align: center
-  text-transform: uppercase
-
-form
-  margin: auto
-  width: 16pc
-
-label
-  font-family: 'Courier New', Courier, monospace
-  font-size: 0.9em
-  font-style: italic
-
-input
-  background-color: #946064
-  border: solid 1px
-  border-radius: 5px
-  padding: 8px 15px
-  text-align: center
-  width: 80%
-
-input[type="number"]
-  appearance: textfield
-
 .temp-area
-  margin-top: 2vh
+  margin: 2vh auto auto auto
+  width: 80%
 
 .temp-inputs
   display: flex
   flex-direction: row
+  justify-content: center
 
 .increase-button
   border-radius: 0px
+  border-right-style: none
+  font-weight: bold
 
 .decrease-button
   border-radius: 0 4px 4px 0
+  font-weight: bold
 
 .increase-button, .decrease-button
   width: 14%
@@ -217,26 +169,22 @@ input[type="number"]
 #buttonArea
   display: flex
   justify-content: center
-  margin: auto
+  margin: 2pc auto auto auto
 
 #botao
-  background-color: #946064
-  border: 1px solid
-  border-radius: 4px
-  font-size: 18px
+  background-color: #f3f2f2
+  border: 1px solid #ddc9cf 
+  border-radius: 10px
+  font-size: 16pt
+  font-weight: bold
   padding: 10px
   cursor: pointer
   display: inherit
   justify-content: inherit
   margin-top: 4vh
+  @apply bg-rose-50 dark:bg-stone-500
 
 #fahrenheit, #celsius, #kelvin
   border-radius: 4px 0 0 4px
 
-::placeholder
-  color: #f39ba3
-
-::selection
-  background-color: #f39ba2a4
-  color: #f0839acb
 </style>
