4b164af83b61ee1557dd975f57bcc197acaa20f0
igorbkuhl <igorb.kuhl@gmail.com>
2023-09-21 13:25:40 -0300

    Added styles and divs.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -25,25 +25,30 @@ const ToggleMenu = () => {
 
 		<h3>Menu</h3>
 		<div class="menu">
-			<router-link class="button" to="/">
-				<span class="icon">
-					<i class="fa-solid fa-house"></i>
-				</span>
-				<span class="text">Home</span>
-			</router-link>
-			<hr>
-			<router-link class="button" to="/Atividade_JS_1">
-				<span class="icon">
-					<i class="fa-solid fa-house"></i>
-				</span>
-				<span class="text">Home</span>
-			</router-link>
-			<router-link class="button" to="/Atividade_JS_2">
-				<span class="icon">
-					<i class="fa-solid fa-house"></i>
-				</span>
-				<span class="text">Home</span>
-			</router-link>
+      <div class="home-link activity-link">
+			  <router-link class="button" to="/">
+			  	<span class="icon">
+			  		<i class="fa-solid fa-house"></i>
+			  	</span>
+			  	<span class="text">Home</span>
+			  </router-link>
+      </div>
+      <div class="temp-link activity-link">
+		  	<router-link class="button" to="/Atividade_JS_1">
+		  		<span class="icon">
+		  			<i class="fa-solid fa-temperature-three-quarters"></i>
+		  		</span>
+		  		<span class="text temp-text">Conversor</span>
+		  	</router-link>
+      </div>
+      <div class="vote-link activity-link">
+		  	<router-link class="button" to="/Atividade_JS_2">
+		  		<span class="icon">
+		  			<i class="fa-solid fa-users"></i>
+		  		</span>
+		  		<span class="text">Contagem de votos</span>
+		  	</router-link>
+      </div>
 		</div>
 		<div class="dark-mode-area">
 			<DarkButton />
@@ -51,108 +56,147 @@ const ToggleMenu = () => {
 	</aside>
 </template>
 
-<style lang="sass" scoped>
-aside
-    display: flex
-    flex-direction: column
-    width: calc(2rem + 32px)
-    min-height: 100vh
-    overflow: hidden // Is hidden when gets out of screen
-    padding: 1rem
+<style lang="scss" scoped>
+aside {
+  display: flex;
+  flex-direction: column;
+  width: calc(2rem + 32px);
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 1rem;
 
-    background-color: var(--dark)
-    color: var(--light)
+  background-color: var(--dark);
+  color: var(--light);
 
-    transition: 0.2s ease-out
+  transition: 0.2s ease-out;
 
-    .logo
-        margin-bottom: 1rem
+  .logo {
+    margin-bottom: 1rem;
 
-        img
-            width: 2rem
+    img {
+      width: 2rem;
+    }
+  }
+  
+  .menu-toggle-wrap {
+    display: flex;
+    margin-bottom: 1rem;
 
-    .menu-toggle-wrap
-        display: flex
-        justify-content: flex-end
-        margin-bottom: 1rem
+    position: relative;
+    top: 0;
+    transition: 0.2s ease-out;
 
-        position: relative
-        top: 0
-        transition: 0.2s ease-out
+    .menu-toggle {
+      transform: 0.2s ease-out;
+    }
+      
+      .icon .fa-angles-right {
+          font-size: 1rem;
+          color: var(--light);
+          transition: 0.2s ease-out;
+          transform: translateX(0.3rem);
+      }
+      
+      &:hover .icon .fa-angles-right {
+        color: var(--primary);
+        transform: translateX(0.4rem);
+      }
+  }
 
-        .menu-toggle
-            transition: 0.2s ease-out
+  h3, .button .text {
+    opacity: 0;
+    transition: 0.3s ease-out;
+  }
 
-            .material-icons
-                font-size: 2rem
-                color: var(--light)
-                transition: 0.2s ease-out
+  h3 {
+    color: var(--grey);
+    font-size: 0.875rem;
+    text-transform: uppercase;
+  }
 
-            &:hover
-                .material-icons
-                    color: var(--primary)
-                    transform: translateX(0.5rem)
+  .menu {
+    margin: 0 -1rem;
 
-    h3, .button .text
-        opacity: 0
-        transition: 0.3s ease-out
+    .home-link {
+      margin-bottom: 1rem;
+    }
+    .activity-link {
+      
+      height: 3.2rem;
+    }
 
-    h3
-        color: var(--grey)
-        font-size: 0.875rem
-        margin-bottom: 0.5rem
-        text-transform: uppercase
+    .button {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      height: inherit;
 
-    .menu
-        margin: 0 -1rem
+      padding: 0.3rem 1rem;
+      transition: 0.2s ease-out;
 
-        .button
-            display: flex
-            align-items: center
-            text-decoration: none
+      .fa-solid {
+        font-size: 1.5rem;
+        color: var(--light);
+        height: inherit;
+        width: 30px;
+        transition: 0.2s ease-out;
+      }
+      .fa-temperature-three-quarters {
+        transform: translateX(0.4rem);
+      }
+      .fa-users {
+        transform: translateX(-0.08rem);
+      }
 
-            padding: 0.5rem 1rem
-            transition: 0.2s ease-out
+      .text {
+        color: var(--light);
+      }
 
-            .material-icons
-                font-size: 2rem
-                color: var(--light)
-                transition: 0.2s ease-out
+      &:hover, &.router-link-exact-active {
+        background-color: var(--dark-alt);
 
-            .text
-                color: var(--light)
+        .fa-solid, .text {
+          color: var(--primary);
+        }
+      }
+      
+      &.router-link-exact-active {
+        border-right: 5px solid var(--primary);
+      }
+    }
+  }
 
-            &:hover, &.router-link-exact-active
-                background-color: var(--dark-alt)
+  &.is-expanded {
+    width: var(--sidebar-width);
 
-                .material-icons, .text
-                    color: var(--primary)
+    .menu-toggle-wrap {
 
-            &.router-link-exact-active
-                border-right: 5px solid var(--primary)
+      .menu-toggle {
+        transform: rotate(-180deg) translateX(-0.3rem);
+      }
+    }
 
-    &.is-expanded
-        width: var(--sidebar-width)
+    h3, .button .text {
+      opacity: 1;
+    }
 
-        .menu-toggle-wrap
-            top: -3rem
+    .button .fa-solid {
+      margin-right: 1rem;
+    }
+    .button {
+      padding: 0.4rem 1rem;
+    }
+  }
 
-            .menu-toggle
-                transform: rotate(-180deg)
+  @media (max-width: 768px) {
+    position: fixed;
+    z-index: 99;
+  }
 
-        h3, .button .text
-            opacity: 1
-
-        .button
-            .material-icons
-                margin-right: 1rem
-
-    @media (max-width: 768px)
-        position: fixed
-        z-index: 99
-
-hr
-    margin-bottom: 2rem
-
-    border: solid 1px var(--dark)
+  .dark-mode-area {
+    display: flex;
+    justify-content: flex-start;
+    margin: 9rem -1rem 0 -1rem;
+  }
+}
 </style>
