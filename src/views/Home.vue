304f1beebc4119cc5091f6357425e30139e94c5a
igorbkuhl <igorb.kuhl@gmail.com>
2023-10-01 20:24:23 -0300

    Added links to home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,79 @@
 <template>
-    <main class="home-page">
-        <h1>Home</h1>
-        <p>This is the home page</p>
-    </main>
+	<main class="home-page">
+		<h1>Home</h1>
+		<div class="links">
+			<div class="js-links">
+				<h2>JavaScript</h2>
+				<router-link class="link" to="/Atividade_JS_1">Conversor de Temperaturas</router-link>
+				<router-link class="link" to="/Atividade_JS_2">Contagem de Votos</router-link>
+				<router-link class="link" to="/Atividade_JS_3">Detector de Anos bissextos (em breve)</router-link>
+				<router-link class="link" to="/Atividade_JS_4">Calculadora Prompt (em breve)</router-link>
+				<router-link class="link" to="/Atividade_JS_5">Caixas Premiadas (em breve)</router-link>
+				<router-link class="link" to="/Atividade_JS_6">Sorteio (em breve)</router-link>
+				<router-link class="link" to="/Atividade_JS_7">Cifra de César (em breve)</router-link>
+				<router-link class="link" to="/Atividade_JS_8">Boletim (em breve)</router-link>
+				<router-link class="link" to="/Atividade_JS_9">Idades (em breve)</router-link>
+				<router-link class="link" to="/Atividade_JS_10">Bloco Dinâmico (em breve)</router-link>
+			</div>
+			<div class="node-links">
+				<h2>NodeJS</h2>
+				<router-link class="link" to="/Atividade_Node_1">Tabuada (em breve)</router-link>
+				<router-link class="link" to="/Atividade_Node_2">Perímetro de Circunferência (em breve)</router-link>
+				<router-link class="link" to="/Atividade_Node_3">Área de Circunferência (em breve)</router-link>
+				<router-link class="link" to="/Atividade_Node_4">Fatorial (em breve)</router-link>
+				<router-link class="link" to="/Atividade_Node_5">RegEx (em breve)</router-link>
+			</div>
+		</div>
+	</main>
 </template>
+
+<style lang="scss">
+h1 {
+	text-align: left;
+}
+
+.links {
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+
+	.js-links {
+		display: flex;
+		flex-direction: column; margin: 1rem;
+
+		.link {
+			padding: 0.5em;
+		}
+		.link:hover {
+			@apply bg-[#f7bcc985] dark:bg-[#7a6570] rounded-md
+		}
+
+		h2 {
+			margin-bottom: 1rem;
+			font-size: 20pt;
+			text-decoration: underline;
+		}
+	}
+
+	.node-links {
+		display: flex;
+		flex-direction: column;
+		margin: 1rem;
+
+		h2 {
+			margin-bottom: 1rem;
+			font-size: 20pt;
+			text-decoration: underline;
+		}
+
+		.link {
+			padding: 0.5em;
+		}
+		.link:hover {
+			// background-color: rgb(122, 101, 112);
+			// border-radius: 8px;
+			@apply bg-[#f7bcc985] dark:bg-[#7a6570] rounded-md
+		}
+	}
+}
+</style>
