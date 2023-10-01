8f0109ccee73fb95b64038f935ef9f665ff72b26
igorbkuhl <igorb.kuhl@gmail.com>
2023-10-01 20:26:33 -0300

    Added customization and total votes.

--- src/views/JavaScript/Atividade_JS_2.vue
+++ src/views/JavaScript/Atividade_JS_2.vue
@@ -31,38 +31,63 @@ export default defineComponent({
 <template>
   <main>
     <div class="main-area">
-      <h1>Total de votos</h1>
+      <h1>Contagem de votos</h1>
       <div class="vote-area">
         <div class="electors">
           <div class="votes">
             <h2>Regis da Costa</h2>
-            <p class="percentage">{{ `${calculatePercentage(regisDaCosta, calculateTotal(total))} %` }}</p>
+            <p class="percentage">
+              <span>{{ regisDaCosta }}</span>
+              {{ ` (${calculatePercentage(regisDaCosta, calculateTotal(total))}%)` }}
+            </p>
           </div>
           <div class="votes">
             <h2>Paulo Machado</h2>
-            <p class="percentage">{{ `${calculatePercentage(pauloMachado, calculateTotal(total))} %` }}</p>
+            <p class="percentage">
+              <span>{{ pauloMachado }}</span>
+              {{ ` (${calculatePercentage(pauloMachado, calculateTotal(total))}%)` }}
+            </p>
           </div>
           <div class="votes">
             <h2>Maria Pinho</h2>
-            <p class="percentage">{{ `${calculatePercentage(mariaPinho, calculateTotal(total))} %` }}</p>
+            <p class="percentage">
+              <span>{{ mariaPinho }}</span>
+              {{ ` (${calculatePercentage(mariaPinho, calculateTotal(total))}%)` }}
+            </p>
           </div>
           <div class="votes">
             <h2>Angela Martins</h2>
-            <p class="percentage">{{ `${calculatePercentage(angelaMartins, calculateTotal(total))} %` }}</p>
+            <p class="percentage">
+              <span>{{ angelaMartins }}</span>
+              {{ ` (${calculatePercentage(angelaMartins, calculateTotal(total))}%)` }}
+            </p>
           </div>
           <div class="votes">
             <h2>Mateus Silvério</h2>
-            <p class="percentage">{{ `${calculatePercentage(mateusSilverio, calculateTotal(total))} %` }}</p>
+            <p class="percentage">
+              <span>{{ mateusSilverio }}</span>
+              {{ ` (${calculatePercentage(mateusSilverio, calculateTotal(total))}%)` }}
+            </p>
           </div>
         </div>
         <div class="other-votes">
           <div class="votes">
-            <h3>Votos brancos</h3>
-            <p class="percentage">{{ `${calculatePercentage(blancVotes, calculateTotal(total))} %` }}</p>
+            <h2>Votos brancos</h2>
+            <p class="percentage">
+              <span>{{ blancVotes }}</span>
+              {{ ` (${calculatePercentage(blancVotes, calculateTotal(total))}%)` }}
+            </p>
           </div>
           <div class="votes">
-            <h3>Votos nulos</h3>
-            <p class="percentage">{{ `${calculatePercentage(nullVotes, calculateTotal(total))} %` }}</p>
+            <h2>Votos nulos</h2>
+            <p class="percentage">
+              <span>{{ nullVotes }}</span>
+              {{ ` (${calculatePercentage(nullVotes, calculateTotal(total))}%)` }}
+            </p>
+          </div>
+          <div class="total-votes">
+            <h2>Votos totais</h2>
+            <p class="votes">{{ `${total} votos` }}</p>
           </div>
         </div>
       </div>
@@ -89,6 +114,13 @@ export default defineComponent({
 
       .votes {
         margin-bottom: 10px;
+
+        h2 {
+          font-weight: bold;
+        }
+        p {
+          font-style: italic;
+        }
       }
     }
     .other-votes {
@@ -96,7 +128,28 @@ export default defineComponent({
       display: flex;
       flex-direction: column;
       justify-content: right;
-      margin-bottom: 10px;
+
+      .votes {
+        margin-bottom: 10px;
+        
+        h2 {
+          font-weight: bold;
+        }
+        p {
+          font-style: italic;
+        }
+      }
+
+      .total-votes {
+        margin-top: 7em;
+
+        h2 {
+          font-weight: bold;
+        }
+        p {
+          font-style: italic;
+        }
+      }
     }
   }
 }
