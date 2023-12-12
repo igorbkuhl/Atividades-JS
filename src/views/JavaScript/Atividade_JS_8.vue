191b6cba95097065189a348c93fafdc2de30b246
igorbkuhl <igorb.kuhl@gmail.com>
2023-12-12 15:36:31 -0300

    Average calculation working

--- src/views/JavaScript/Atividade_JS_8.vue
+++ src/views/JavaScript/Atividade_JS_8.vue
@@ -1,55 +1,49 @@
-<script lang="ts">
-import { defineComponent } from "vue";
+<script lang="ts" setup>
+let studentList = [
+  {
+    id: 0,
+    label: "",
+    grade1: 0,
+    grade2: 0,
+    average: 0,
+    freq: 0,
+  },
+];
+let count = 1;
 
-export default defineComponent({
-  data() {
-    return {
-      studentList: [{
-        id: 0,
-        label: "",
-        grade1: 0,
-        grade2: 0,
-        average: 0,
-        freq: 0,
-      }],
-      count: 0,
-    };
-  },
-  methods: {
-    avg(g1: number, g2: number) {
-      return (g1 + g2) / 2;
+const avg = (g1: number, g2: number) => (g1 + g2) / 2;
+
+function signup() {
+  studentList = [
+    {
+      id: 0,
+      label: "",
+      grade1: 0,
+      grade2: 0,
+      average: 0,
+      freq: 0,
     },
-    signup() {
-      this.studentList = [{
-        id: 0,
-        label: "",
-        grade1: 0,
-        grade2: 0,
-        average: 0,
-        freq: 0,
-      }];
-      while (true) {
-        this.studentList.push({
-          id: this.count + 1,
-          label: prompt(
-            `Insira o nome do ${this.count + 1}º aluno.`
-          ),
-          grade1: Number(prompt("Insira a nota do primeiro semestre.")),
-          grade2: Number(prompt("Insira a nota do segundo semestre.")),
-          average: this.avg(
-            this.studentList[this.count].grade1,
-            this.studentList[this.count].grade2
-          ),
-          freq: Number(prompt("Insira a frequência.")),
-        });
-        if (!confirm("Deseja adicionar mais um aluno?")) {
-          break;
-        }
-        this.count++;
-      }
-    },
-  },
-});
+  ];
+
+  while (true) {
+    let labelValue = String(prompt(`Insira o nome do ${count}º aluno.`));
+    let grade1Value = Number(prompt("Insira a nota do primeiro semestre."));
+    let grade2Value = Number(prompt("Insira a nota do segundo semestre."));
+
+    studentList.push({
+      id: count,
+      label: labelValue,
+      grade1: grade1Value,
+      grade2: grade2Value,
+      average: avg(grade1Value, grade2Value),
+      freq: Number(prompt("Insira a frequência.")),
+    });
+    if (!confirm("Deseja adicionar mais um aluno?")) {
+      break;
+    }
+    count++;
+  }
+}
 </script>
 
 <template>
@@ -102,17 +96,16 @@ export default defineComponent({
     text-align: center;
   }
 
-  table
-    * {
-      border: #000 1px solid;
-      padding: 2px;
-      text-align: center;
-    }
-    thead tr th:nth-child(1) {
-      width: 2em;
-    }
-    tbody tr:nth-child(1) {
-      display: none;
-    }
+  table * {
+    border: #000 1px solid;
+    padding: 2px;
+    text-align: center;
+  }
+  thead tr th:nth-child(1) {
+    width: 2em;
+  }
+  tbody tr:nth-child(1) {
+    display: none;
+  }
 }
 </style>
