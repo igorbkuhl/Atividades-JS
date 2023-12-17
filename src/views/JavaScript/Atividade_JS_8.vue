b63f3c4f94a0a2975b627ab56217666f12224dfe
igorbkuhl <igorb.kuhl@gmail.com>
2023-12-17 17:43:38 -0300

    Average defines table row background color

--- src/views/JavaScript/Atividade_JS_8.vue
+++ src/views/JavaScript/Atividade_JS_8.vue
@@ -2,54 +2,66 @@
 let studentList = [
   {
     id: 0,
-    label: "",
+    name: "",
     grade1: 0,
     grade2: 0,
     average: 0,
     freq: 0,
+    approved: false,
   },
 ];
 let count = 1;
 
 const avg = (g1: number, g2: number) => (g1 + g2) / 2;
 
-function signup() {
-  studentList = [
-    {
-      id: 0,
-      label: "",
-      grade1: 0,
-      grade2: 0,
-      average: 0,
-      freq: 0,
-    },
-  ];
+studentList = [
+  {
+    id: 0,
+    name: "",
+    grade1: 0,
+    grade2: 0,
+    average: 0,
+    freq: 0,
+    approved: false,
+  },
+];
 
-  while (true) {
-    let labelValue = String(prompt(`Insira o nome do ${count}º aluno.`));
-    let grade1Value = Number(prompt("Insira a nota do primeiro semestre."));
-    let grade2Value = Number(prompt("Insira a nota do segundo semestre."));
+while (true) {
+  let nameValue = String(prompt(`Insira o nome do ${count}º aluno.`));
+  let grade1Value = Number(prompt("Insira a nota do primeiro semestre."));
+  let grade2Value = Number(prompt("Insira a nota do segundo semestre."));
+  let avgValue = avg(grade1Value, grade2Value);
+  let freqValue = Number(prompt("Insira a frequência."));
 
-    studentList.push({
-      id: count,
-      label: labelValue,
-      grade1: grade1Value,
-      grade2: grade2Value,
-      average: avg(grade1Value, grade2Value),
-      freq: Number(prompt("Insira a frequência.")),
-    });
-    if (!confirm("Deseja adicionar mais um aluno?")) {
-      break;
-    }
-    count++;
+  studentList.push({
+    id: count,
+    name: nameValue,
+    grade1: grade1Value,
+    grade2: grade2Value,
+    average: avgValue,
+    freq: freqValue,
+    approved: false,
+  });
+
+  if (avgValue >= 7 && freqValue >= 70) {
+    studentList[count].approved = true;
   }
+
+  studentList[1].name == "null"
+    ? studentList.pop()
+    : console.log("Aluno adicionado");
+
+  if (!confirm("Deseja adicionar mais um aluno?")) {
+    break;
+  }
+  count++;
 }
 </script>
 
 <template>
   <main>
     <div class="main-area">
-      <h1 @click="signup">Boletim</h1>
+      <h1>Boletim</h1>
       <table>
         <thead>
           <tr>
@@ -62,12 +74,19 @@ function signup() {
           </tr>
         </thead>
         <tbody>
-          <tr v-for="student in studentList">
+          <tr
+            v-for="student in studentList"
+            :class="`${
+              student.approved
+                ? 'bg-[#9edb9e] !text-[#0f0a0a]'
+                : 'bg-[#df9f9f] !text-[#180101]'
+            }`"
+          >
             <td>
               {{ student.id }}
             </td>
             <td>
-              {{ student.label }}
+              {{ student.name }}
             </td>
             <td>
               {{ student.grade1 }}
@@ -104,8 +123,19 @@ function signup() {
   thead tr th:nth-child(1) {
     width: 2em;
   }
-  tbody tr:nth-child(1) {
-    display: none;
+  tbody {
+    tr:nth-child(1) {
+      display: none;
+    }
+
+    tr.approved-student {
+      background-color: #9edb9e;
+      color: #0f0a0a !important;
+    }
+    tr.disapproved-student {
+      background-color: #df9f9f;
+      color: #180101 !important;
+    }
   }
 }
 </style>
